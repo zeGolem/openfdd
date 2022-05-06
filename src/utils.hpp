cdbcf9d6ad9fbb579ba1d44c7b5355a3a95f6f86
zeGolem <zegolem1383@outlook.fr>
2022-05-06 11:25:55 +0200

    utils: Added scale() util
    This makes it easy to scale values from one range to another, using
    fancy math magic

--- src/utils.hpp
+++ src/utils.hpp
@@ -24,4 +24,12 @@ void ensure_range(auto        value,
 		    std::to_string(min) + " and " + std::to_string(max));
 }
 
+constexpr auto scale(
+    auto value, auto from_min, auto from_max, auto to_min, auto to_max)
+{
+	// Fancy math stuff that I don't really know how to explain, but it works!
+	return (((value - from_min) / (from_max - from_min)) * (to_max - to_min)) +
+	       to_min;
+}
+
 } // namespace utils
