3b5fa05ef94a7da91f2f2b2036c5c4277c6f8206
zeGolem <zegolem1383@outlook.fr>
2022-05-13 12:42:28 +0200

    drivers/manager: Created driver manager
    This class behaves like usb::device_manager, but for drivers. It will
    make implementing hotplug much easier to have this wrapper.

---
+++ src/drivers/manager.cpp
@@ -0,0 +1,38 @@
+#include "manager.hpp"
+#include "drivers/driver.hpp"
+#include "drivers/steelseries/aerox_3_wireless.hpp"
+#include "drivers/steelseries/apex_100.hpp"
+#include "drivers/steelseries/rival_3_wireless.hpp"
+#include "usb.hpp"
+#include <memory>
+
+namespace drivers
+{
+
+std::optional<std::shared_ptr<driver>> manager::create_driver_if_available(
+    std::shared_ptr<usb_device> device) const
+{
+#define CHECK_DRIVER(drv)                                                      \
+	if (drv::is_compatible(device))                                            \
+	return std::make_shared<drv>(device, m_config_manager)
+
+	CHECK_DRIVER(steelseries::aerox_3_wireless);
+	CHECK_DRIVER(steelseries::rival_3_wireless);
+	CHECK_DRIVER(steelseries::apex_100);
+
+	return {};
+}
+
+identifiable_driver_map manager::create_drivers_for_available_devices() const
+{
+	identifiable_driver_map map = {};
+
+	for (auto const& [identifier, device] : m_device_manager.devices()) {
+		auto const& new_driver = create_driver_if_available(device);
+		if (new_driver.has_value()) map[identifier] = new_driver.value();
+	}
+
+	return map;
+}
+
+} // namespace drivers
---
+++ src/drivers/manager.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "config.hpp"
+#include "drivers/driver.hpp"
+#include "usb.hpp"
+#include "usb/device_manager.hpp"
+#include <functional>
+#include <memory>
+#include <optional>
+#include <unordered_map>
+#include <utility>
+namespace drivers
+{
+
+typedef std::unordered_map<usb_device::identifier, std::shared_ptr<driver>>
+    identifiable_driver_map;
+
+class manager
+{
+  public:
+	manager(usb::device_manager& device_manager)
+	    : m_device_manager(device_manager),
+	      m_config_manager(std::make_shared<config_manager>(
+	          config_manager::get_default_filepath()))
+	{
+	}
+
+	std::optional<std::shared_ptr<driver>> create_driver_if_available(
+	    std::shared_ptr<usb_device> device) const;
+
+	identifiable_driver_map create_drivers_for_available_devices() const;
+
+	void start_hotplug_support(
+	    std::function<void(identifiable_driver_map new_driver_list)>
+	        driver_list_updated_callback);
+
+  private:
+	usb::device_manager&            m_device_manager;
+	std::shared_ptr<config_manager> m_config_manager;
+};
+
+} // namespace drivers
