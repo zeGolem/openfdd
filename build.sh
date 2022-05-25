2c46c7c60f2db839ae50d2404543d32eab219867
zegolem <zegolem1383@outlook.fr>
2022-05-25 20:33:12 +0200

    build: Added easy way to switch compiler
    You can now set the CXX environement variable to switch to a different
    compiler :)

--- build.sh
+++ build.sh
@@ -5,26 +5,27 @@ set -ex;
 # TODO: Better build system?
 
 SOURCES="src/*.cpp src/usb/*.cpp src/drivers/*.cpp src/drivers/*/*.cpp"
+CXX=${CXX:-g++}
 
 build_dev() {
-	g++ $SOURCES                     \
-		-lusb-1.0                    \
-		-Isrc/                       \
-		-fsanitize=address,undefined \
-		-Wall -Wextra                \
-		-std=c++20 -pedantic         \
-		-g                           \
-		-o openfdd
+	$CXX $SOURCES                     \
+		 -lusb-1.0                    \
+		 -Isrc/                       \
+		 -fsanitize=address,undefined \
+		 -Wall -Wextra                \
+		 -std=c++20 -pedantic         \
+		 -g                           \
+		 -o openfdd
 }
 
 build_release() {
-	g++ $SOURCES                     \
-		-lusb-1.0                    \
-		-Isrc/                       \
-		-Wall -Wextra                \
-		-std=c++20 -pedantic         \
-		-O2                          \
-		-o openfdd
+	$CXX $SOURCES                     \
+		 -lusb-1.0                    \
+		 -Isrc/                       \
+		 -Wall -Wextra                \
+		 -std=c++20 -pedantic         \
+		 -O2                          \
+		 -o openfdd
 }
 
 if [ "$1" = "dev" ]; then
