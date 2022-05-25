7fad359b581a9473e01346b6b92d9e0e0d19a7fd
zegolem <zegolem1383@outlook.fr>
2022-05-25 20:18:09 +0200

    build: Now default to making a release build
    `./build.sh dev` makes a debug build.
    
    Having a separation between both is, I think, a good idea

--- build.sh
+++ build.sh
@@ -6,11 +6,29 @@ set -ex;
 
 SOURCES="src/*.cpp src/usb/*.cpp src/drivers/*.cpp src/drivers/*/*.cpp"
 
-g++ $SOURCES                     \
-	-lusb-1.0                    \
-	-Isrc/                       \
-	-fsanitize=address,undefined \
-	-Wall -Wextra                \
-	-std=c++20 -pedantic         \
-	-g                           \
-	-o openfdd
+build_dev() {
+	g++ $SOURCES                     \
+		-lusb-1.0                    \
+		-Isrc/                       \
+		-fsanitize=address,undefined \
+		-Wall -Wextra                \
+		-std=c++20 -pedantic         \
+		-g                           \
+		-o openfdd
+}
+
+build_release() {
+	g++ $SOURCES                     \
+		-lusb-1.0                    \
+		-Isrc/                       \
+		-Wall -Wextra                \
+		-std=c++20 -pedantic         \
+		-O2                          \
+		-o openfdd
+}
+
+if [ "$1" = "dev" ]; then
+	build_dev;
+else
+	build_release;
+fi;
